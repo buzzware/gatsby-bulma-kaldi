95bf7e5c7f04cb023a9d8f457ac39459b90247ed
Gary McGhee <gary@huey.co>
2021-12-14 12:52:43 +0800

    further stripped down

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Gatsby Bulma Kaldi",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Example Gatsby and Bulma CSS project, based on https://github.com/netlify-templates/gatsby-starter-netlify-cms",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
